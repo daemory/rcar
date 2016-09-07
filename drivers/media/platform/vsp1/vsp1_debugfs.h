b7421484edd202f35dccb9516c9d69017c2a938a
Kieran Bingham <kieran+renesas@bingham.xyz>
2016-09-07 09:41:53 +0100

    v4l: vsp1: Add debugfs system v1.4
    Provide a directory under the debugfs filesystem where extra
    information can be requested when debugging the hardware.
    
    Status information for the WPF is presented under info.
    Registers can be inspected in regs
    
    Signed-off-by: Kieran Bingham <kieran+renesas@bingham.xyz>
    
    Changelog:
    
    Earlier versions were not tracked.
    
    v1.1
     - Fix the regs_local vsp1 pointer that is passed to get the device
       references
    
    v1.2
     - Fix up the RPF extension so that it displays the RPF number on those
       specific regset entries
    
    v1.3
    - Fix regs_local buffer size (Currently consuming at least 6k)
    - Add a 'reset wpf' hook
    - Add in VI6_CMD register reads
    - Add in MRESET and Dynamic clock control registers
    - Provide decode for vi6_disp_irq_sta
    - Remove LookUpTable registers as they are not available on all cells
    - Provide IRQ Enable status as well as level status
    
    v1.4
    - Move debugfs init/clean to probe/remove
    - use devname(dev) to create the VSP debugfs root dir
    - Remove 'Hard PM lock' which kept VSP's permanantly clocked

---
+++ drivers/media/platform/vsp1/vsp1_debugfs.h
@@ -0,0 +1,37 @@
+/*
+ * vsp1_debugfs.h  --  R-Car VSP1 DebugFS system
+ *
+ * Copyright (C) 2016 Renesas Corporation
+ *
+ * Contact: Kieran Bingham (kieran@bingham.xyz)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef __VSP1_DEBUGFS_H__
+#define __VSP1_DEBUGFS_H__
+
+#ifdef CONFIG_DEBUG_FS
+int vsp1_debugfs_init(struct vsp1_device *vsp1);
+void vsp1_debugfs_remove(struct vsp1_device *vsp1);
+char *vsp1_reg_to_name(u32 offset);
+#else
+static inline int vsp1_debugfs_init(struct vsp1_device *vsp1)
+{
+	return 0;
+}
+
+static inline void vsp1_debugfs_remove(struct vsp1_device *vsp1)
+{
+	return;
+}
+
+static inline char *vsp1_reg_to_name(u32 offset) {
+	return "<>";
+}
+#endif /* CONFIG_DEBUG_FS */
+
+#endif /* __VSP1_DEBUGFS_H__ */
