17d11e896937e7fde763d24261fd589d95fe7cf2
Kieran Bingham <kieran+renesas@bingham.xyz>
2016-09-09 19:12:36 +0100

    v4l: vsp1: Provide WPF underflow error detection and reporting
    In erroneous conditions, the WPF can assert an underrun interrupt. This
    usually represents a mis-configured pipeline, or bus priority issue. It
    is likely that in the event of this interrupt being asserted the
    hardware is no longer operational. A device error message at the least
    is useful in this scenario
    
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>

--- drivers/media/platform/vsp1/vsp1_wpf.c
+++ drivers/media/platform/vsp1/vsp1_wpf.c
@@ -351,7 +351,7 @@ static void wpf_configure(struct vsp1_entity *entity,
 	/* Enable interrupts */
 	vsp1_dl_list_write(dl, VI6_WPF_IRQ_STA(wpf->entity.index), 0);
 	vsp1_dl_list_write(dl, VI6_WPF_IRQ_ENB(wpf->entity.index),
-			   VI6_WFP_IRQ_ENB_DFEE);
+			   VI6_WFP_IRQ_ENB_DFEE | VI6_WFP_IRQ_ENB_UNDE);
 }
 
 static const struct vsp1_entity_operations wpf_entity_ops = {
