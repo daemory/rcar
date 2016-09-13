kbingham/linux

DNI: Add dprintk

drivers/media/platform/vsp1/vsp1_drv.c
@@ -40,6 +40,10 @@
 #include "vsp1_uds.h"
 #include "vsp1_video.h"
 
+
+int vsp1_debug = DEBUG_DL_LIST;
+module_param(vsp1_debug, int, 0644);
+
 /* -----------------------------------------------------------------------------
  * Interrupt Handling
  */

daemory/rcar

drivers/media/platform/vsp1/vsp1_drv.c
@@ -40,6 +40,10 @@
 #include "vsp1_uds.h"
 #include "vsp1_video.h"
 
+
+int vsp1_debug = DEBUG_INFO;
+module_param(vsp1_debug, int, 0644);
+
 /* -----------------------------------------------------------------------------
  * Debugfs management
  */