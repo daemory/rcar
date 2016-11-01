c312ea29a678f418bc945d13782d3c6466239579
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2016-11-01 11:16:00 +0000

    v4l: vsp1: Calculate partition sizes at stream start.
    Previously the active window and partition sizes for each partition were
    calculated for each partition every frame. This data is constant and
    only needs to be calculated once at the start of the stream.
    
    Extend the vsp1_pipe object to dynamically store the number of partitions
    required and pre-calculate the partition sizes into this table.
    
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    
    ---
    v2:
     - Partition table allocated dynamically as required.
     - loop uses unsigned int
     - rebase on top of suspend-resume fix commits

--- drivers/media/platform/vsp1/vsp1_video.c
+++ drivers/media/platform/vsp1/vsp1_video.c
@@ -257,12 +257,13 @@ static struct v4l2_rect vsp1_video_partition(struct vsp1_pipeline *pipe,
 	return partition;
 }
 
-static void vsp1_video_pipeline_setup_partitions(struct vsp1_pipeline *pipe)
+static int vsp1_video_pipeline_setup_partitions(struct vsp1_pipeline *pipe)
 {
 	struct vsp1_device *vsp1 = pipe->output->entity.vsp1;
 	const struct v4l2_mbus_framefmt *format;
 	struct vsp1_entity *entity;
 	unsigned int div_size;
+	unsigned int i;
 
 	/*
 	 * Partitions are computed on the size before rotation, use the format
@@ -275,9 +276,15 @@ static void vsp1_video_pipeline_setup_partitions(struct vsp1_pipeline *pipe)
 
 	/* Gen2 hardware doesn't require image partitioning. */
 	if (vsp1->info->gen == 2) {
+		pipe->part_table = kcalloc(1, sizeof(*pipe->part_table),
+					   GFP_ATOMIC);
+		if (pipe->part_table == NULL)
+			return -ENOMEM;
+
 		pipe->div_size = div_size;
 		pipe->partitions = 1;
-		return;
+		pipe->part_table[0] = vsp1_video_partition(pipe, div_size, 0);
+		return 0;
 	}
 
 	list_for_each_entry(entity, &pipe->entities, list_pipe) {
@@ -292,6 +299,16 @@ static void vsp1_video_pipeline_setup_partitions(struct vsp1_pipeline *pipe)
 
 	pipe->div_size = div_size;
 	pipe->partitions = DIV_ROUND_UP(format->width, div_size);
+
+	pipe->part_table = kcalloc(pipe->partitions, sizeof(*pipe->part_table),
+				   GFP_ATOMIC);
+	if (pipe->part_table == NULL)
+		return -ENOMEM;
+
+	for (i = 0; i < pipe->partitions; i++)
+		pipe->part_table[i] = vsp1_video_partition(pipe, div_size, i);
+
+	return 0;
 }
 /* -----------------------------------------------------------------------------
  * Pipeline Management
@@ -373,8 +390,7 @@ static void vsp1_video_pipeline_run_partition(struct vsp1_pipeline *pipe,
 {
 	struct vsp1_entity *entity;
 
-	pipe->partition = vsp1_video_partition(pipe, pipe->div_size,
-					       pipe->current_partition);
+	pipe->partition = pipe->part_table[pipe->current_partition];
 
 	list_for_each_entry(entity, &pipe->entities, list_pipe) {
 		if (entity->ops->configure)
@@ -919,6 +935,9 @@ static void vsp1_video_stop_streaming(struct vb2_queue *vq)
 		vsp1_dl_list_put(pipe->dl);
 		pipe->dl = NULL;
 	}
+
+	kfree(pipe->part_table);
+	pipe->part_table = NULL;
 	mutex_unlock(&pipe->lock);
 
 	media_pipeline_stop(&video->video.entity);
